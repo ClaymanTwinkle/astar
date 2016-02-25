8c608040f4ceea7bfed0f5a9994ef6f170529364
kesar <claymantwinkle@gmail.com>
2016-02-25 10:22:02 +0800

    A星算分

---
+++ src/Coord.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * ClassName: Coord
+ * 
+ * @Description: 坐标
+ */
+public class Coord
+{
+
+	public int x;
+	public int y;
+
+	public Coord(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == null) return false;
+		if (obj instanceof Coord)
+		{
+			Coord c = (Coord) obj;
+			return x == c.x && y == c.y;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Coord [x=" + x + ", y=" + y + "]";
+	}
+}
